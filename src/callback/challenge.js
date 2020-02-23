5c3c7d61aaa356a9ddb91a84081ba3bc0143f0ca
Cristian Martinez <cristianandres92@gmail.com>
2020-02-22 19:10:17 -0500

    callback challenge resolved

---
+++ src/callback/challenge.js
@@ -0,0 +1,31 @@
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const API = "https://rickandmortyapi.com/api/character/";
+
+function fetchData(urlApi, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', urlApi, true);
+  xhr.onreadystatechange = function(event) {
+    if(xhr.readyState === 4) {
+      if(xhr.status === 200) {
+        callback(null, JSON.parse(xhr.responseText));
+      } else {
+        const error = new Error('Error ' + urlApi);
+        return callback(error, null);
+      }
+    }
+  }
+  xhr.send();
+}
+
+fetchData(API, function(error1, data1) {
+  if(error1) return console.error(error1);
+  fetchData(API + data1.results[0].id, function(error2, data2) {
+    if(error2) return console.error(error2);
+    fetchData(data2.origin.url, function(error3, data3) {
+      if(error3) return console.error(error3);
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    });
+  });
+});
